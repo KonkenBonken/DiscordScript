3d36eac56d0a75c4d16bb2db2bc2f6845b3f7d0d
KonkenBonken <konrad05pettersson@gmail.com>
2022-05-04 23:33:36 +0200

    Made it more readable

--- grammar.js
+++ grammar.js
@@ -1,12 +1,28 @@
 export default [
-	['intents (?<intents>@d+)', ({ intents }) => `const client=new Discord.Client({intents:${intents}});`],
-	['login (?<token>[a-zA-Z@d]{24}@.[a-zA-Z@d]{6}@.[a-zA-Z@d]{27})', ({ token }) => `client.login(${token});`],
-	['on login:', () => [`client.on('ready',async()=>{`, '})']],
-	['on message(@((?<args>(@w+,)*@w+)@))?:', ({ args }) => [`client.on('messageCreate',async(${args||''})=>{`, '})']],
-	['print %s', ({ string }) => `console.log("${string}");`],
-	['print %b', ({ string }) => `console.log(\`${string}\`);`],
-	['send %s in (?<channel>.+)', ({ string, channel }) => `${channel}.send("${string}");`],
-	['send %b in (?<channel>.+)', ({ string, channel }) => `${channel}.send(\`${string}\`);`],
+	['intents (?<intents>@d+)',
+		({ intents }) => `const client=new Discord.Client({intents:${intents}});`
+	],
+	['login (?<token>[a-zA-Z@d]{24}@.[a-zA-Z@d]{6}@.[a-zA-Z@d]{27})',
+		({ token }) => `client.login(${token});`
+	],
+	['on login:',
+		() => [`client.on('ready',async()=>{`, '})']
+	],
+	['on message(@((?<args>(@w+,)*@w+)@))?:',
+		({ args }) => [`client.on('messageCreate',async(${args||''})=>{`, '})']
+	],
+	['print %s',
+		({ string }) => `console.log("${string}");`
+	],
+	['print %b',
+		({ string }) => `console.log(\`${string}\`);`
+	],
+	['send %s in (?<channel>.+)',
+		({ string, channel }) => `${channel}.send("${string}");`
+	],
+	['send %b in (?<channel>.+)',
+		({ string, channel }) => `${channel}.send(\`${string}\`);`
+	],
 ]
 .map(([regex, parse]) => {
 	[
