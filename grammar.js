6e6f9330b3be6abafe9bdf1485c507fea0cade22
KonkenBonken <konrad05pettersson@gmail.com>
2022-05-04 23:32:59 +0200

    Made shorthands for common regex components

--- grammar.js
+++ grammar.js
@@ -1,13 +1,20 @@
-// string = /(['"])(?<string>(?:(?!\1|\\).|\\.)*)\1/
-// backtick = /`(?<string>(?:(?!`|\\).|\\.)*)`/
-
 export default [
-	[/intents\s+(?<intents>\d+)/, ({ intents }) => `const client=new Discord.Client({intents:${intents}});`],
-	[/login\s+(?<token>[a-zA-Z\d]{24}\.[a-zA-Z\d]{6}\.[a-zA-Z\d]{27})/, ({ token }) => `client.login(${token});`],
-	[/on\s+login:/, () => [`client.on('ready',async()=>{`, '})']],
-	[/on\s+message(\((?<args>(\w+,)*\w+)\))?:/, ({ args }) => [`client.on('messageCreate',async(${args||''})=>{`, '})']],
-	[/print\s+(['"])(?<string>(?:(?!\1|\\).|\\.)*)\1/, ({ string }) => `console.log("${string}");`],
-	[/print\s+`(?<string>(?:(?!`|\\).|\\.)*)`/, ({ string }) => `console.log(\`${string}\`);`],
-	[/send\s+(['"])(?<string>(?:(?!\1|\\).|\\.)*)\1\s+in\s+(?<channel>.+)/, ({ string, channel }) => `${channel}.send("${string}")`],
-	[/send\s+`(?<string>(?:(?!`|\\).|\\.)*)`\s+in\s+(?<channel>.+)/, ({ string, channel }) => `${channel}.send(\`${string}\`)`],
+	['intents (?<intents>@d+)', ({ intents }) => `const client=new Discord.Client({intents:${intents}});`],
+	['login (?<token>[a-zA-Z@d]{24}@.[a-zA-Z@d]{6}@.[a-zA-Z@d]{27})', ({ token }) => `client.login(${token});`],
+	['on login:', () => [`client.on('ready',async()=>{`, '})']],
+	['on message(@((?<args>(@w+,)*@w+)@))?:', ({ args }) => [`client.on('messageCreate',async(${args||''})=>{`, '})']],
+	['print %s', ({ string }) => `console.log("${string}");`],
+	['print %b', ({ string }) => `console.log(\`${string}\`);`],
+	['send %s in (?<channel>.+)', ({ string, channel }) => `${channel}.send("${string}");`],
+	['send %b in (?<channel>.+)', ({ string, channel }) => `${channel}.send(\`${string}\`);`],
 ]
+.map(([regex, parse]) => {
+	[
+		[/\s/g, '\\s+'],
+		[/%s/g, '([\'"])(?<string>(?:(?!\\1|\\\\).|\\\\.)*)\\1'],
+		[/%b/g, '`(?<string>(?:(?!`|\\\\).|\\\\.)*)`'],
+		[/@/g, '\\'],
+	].forEach(([key, exp]) => regex = regex.replace(key, exp));
+
+	return [new RegExp(regex), parse];
+})
